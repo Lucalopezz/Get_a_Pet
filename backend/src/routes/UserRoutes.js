2c8b6b81a78285ae58b3ec530bd43f9b753e9cd6
Lucas <lucaslops12@gmail.com>
2024-01-24 16:11:08 -0300

    end of edit user

--- backend/src/routes/UserRoutes.js
+++ backend/src/routes/UserRoutes.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const router = express.Router(); 
+const express = require("express");
+const router = express.Router();
 
 
-const UserController = require('../controllers/UserController')
 
-router.post('/register', UserController.register)
-router.post('/login', UserController.login)
-router.get('/checkuser', UserController.checkUser)
+const UserController = require("../controllers/UserController");
 
-module.exports = router
+const checkToken = require("../helpers/verify-token");
+
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.get("/checkuser", UserController.checkUser);
+router.get("/:id", UserController.getUserById);
+router.patch("/edit/:id", checkToken , UserController.editUser);
+
+module.exports = router;
