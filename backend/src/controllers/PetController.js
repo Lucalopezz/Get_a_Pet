0fe993ea3a26dab6e8ff84581b8757cfa2f7f3d0
Lucas <lucaslops12@gmail.com>
2024-01-25 19:42:39 -0300

    remove pet

--- backend/src/controllers/PetController.js
+++ backend/src/controllers/PetController.js
@@ -113,4 +113,31 @@ module.exports = class PetController {
 
     res.status(200).json({ pet });
   }
+  static async removePetById(req, res){
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+      res.status(422).json({ message: "Pet não encontrado" });
+      return;
+    }
+
+    const pet = await Pet.findById(id);
+
+    if (!pet) {
+      res.status(404).json({ message: "Pet não encontrado" });
+      return;
+    }
+
+    //chek if logged in user registered the pet
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if(pet.user._id.toString() !== user._id.toString()){
+      res.status(422).json({ message: "Houve um problema em processar sua solicitação! " });
+      return;
+    }
+
+    await Pet.findByIdAndDelete(id)
+    res.status(200).json({message: "Pet removido com sucesso!"})
+  }
 };
