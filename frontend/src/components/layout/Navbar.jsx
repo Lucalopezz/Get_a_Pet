2d1effbd68937e55c28ebd233f97337e32b68aa6
Lucas <lucaslops12@gmail.com>
2024-01-28 13:16:47 -0300

    logout completed

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,10 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import styles from "./Navbar.module.css";
 
 import Logo from "../../assets/img/logo.png";
+import { Context } from "../../context/UserContext";
+import { useContext } from "react";
 
 const Navbar = () => {
+  const navigate = useNavigate()
+  const { authenticated, logout } = useContext(Context);
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_logo}>
@@ -15,12 +19,22 @@ const Navbar = () => {
         <li>
           <Link to="/">Adotar</Link>
         </li>
-        <li>
-          <Link to="/login">Entrar</Link>
-        </li>
-        <li>
-          <Link to="/register">Cadastrar</Link>
-        </li>
+        {authenticated ? (
+          <>
+           <li onClick={(e) => logout(navigate)}>
+            Sair
+           </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Entrar</Link>
+            </li>
+            <li>
+              <Link to="/register">Cadastrar</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
